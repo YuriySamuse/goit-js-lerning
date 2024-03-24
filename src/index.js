4d7f2b445022703e80cd8bfd1f38486e9060f2d0
YuriySamusew <6177385@gmail.com>
2024-03-24 18:00:08 +0100

    filter on input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("Hello");
+// console.log("Hello");
+
+const tech = [
+	{ label: "HTML" },
+	{ label: "CSS" },
+	{ label: "JS" },
+	{ label: "Node" },
+	{ label: "React" },
+	{ label: "Vue" },
+	{ label: "Redux" },
+	{ label: "React Router" },
+	{ label: "MongoDB" },
+];
+
+const refs = {
+	list: document.querySelector(".js-list"),
+	input: document.querySelector("#filter"),
+};
+
+refs.input.addEventListener("input", _.debounce(onFilterChange, 300));
+const listItemsMarkup = createListItemsMarkup(tech);
+populateList(listItemsMarkup);
+
+function createListItemsMarkup(items) {
+	return items.map(item => `<li>${item.label}</li>`).join("");
+}
+
+function onFilterChange(evt) {
+	const filter = evt.target.value;
+	const filteredItems = tech.filter(t => t.label.toLowerCase().includes(filter));
+
+	const listItemsMarkup = createListItemsMarkup(filteredItems);
+	populateList(listItemsMarkup);
+}
+
+function populateList(markap) {
+	refs.list.innerHTML = markap;
+}
