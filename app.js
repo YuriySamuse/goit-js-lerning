e78485e3d55726df300ac3ff11cfd0f9d9b98b5b
YuriySamusew <6177385@gmail.com>
2024-04-01 15:50:46 +0200

    start webpack

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello whith App.js");
+const express = require("express");
+
+const app = express();
+const listener = app.listen(4444, () => {
+	console.log(`webserver started to ${listener.address().port} potr`);
+});
