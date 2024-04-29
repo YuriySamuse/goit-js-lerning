18a1143d9e0f74b33bdddd5f39f8bbafc199ffac
YuriySamusew <6177385@gmail.com>
2024-04-29 12:45:21 +0200

    save input in localstorage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-function component() {
-	const element = document.createElement("div");
-
-	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = _.join(["Hello", "webpack"], " ");
-	element.innerHTML = _.join(`<h1>Hello</h1>`);
-
-	return element;
-}
-
-document.body.appendChild(component());
+import "./css/common.css";
